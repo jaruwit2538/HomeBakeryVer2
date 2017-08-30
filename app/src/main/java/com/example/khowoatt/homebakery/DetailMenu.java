9b9875132cb8d131f9e1d382fc4719bc5d6b1e9a
jaruwit2538 <oatza_first@hotmail.com>
2017-08-30 12:38:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/khowoatt/homebakery/DetailMenu.java
@@ -0,0 +1,40 @@
+package com.example.khowoatt.homebakery;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class DetailMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_menu);
+
+        EditText editText = (EditText) findViewById(R.id.addmenu);
+        Button button = (Button) findViewById(R.id.BtnOrder);
+        ImageView detailimageView = (ImageView) findViewById(R.id.imvDetailmenu);
+        TextView pricetextView = (TextView) findViewById(R.id.pricedetail);
+        TextView nametextView = (TextView) findViewById(R.id.menudetail);
+        TextView detailtextView = (TextView) findViewById(R.id.detail);
+
+        nametextView.setText(getIntent().getStringExtra("name"));
+        pricetextView.setText(getIntent().getStringExtra("price"));
+        detailtextView.setText(getIntent().getStringExtra("detail"));
+        String image = getIntent().getStringExtra("image");
+        Picasso.with(DetailMenu.this).load(image).into(detailimageView);
+    }
+    public void addorder(View view) {
+
+    }
+    public void onClickBack (View view){
+        finish();
+    }
+}
